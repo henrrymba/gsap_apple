139479b517f3472b77810105cee254cb71e9fb20
Henrry Muraña Bautista <henrrymba@icloud.com>
2025-10-21 12:14:21 +0200

    feat(ui): implement showcase section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import { ScrollTrigger } from "gsap/all"
+import gsap from 'gsap';
+import Showcase from "./components/Showcase.jsx";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
     return (
         <main>
             <NavBar />
             <Hero/>
+            <ProductViewer/>
+            <Showcase/>
         </main>
     )
 }
